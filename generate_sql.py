9631aa267fcf1f8f4f8bfc98a11daf7ca1ed11f1
samad409 <heyabdul409@gmail.com>
2026-02-15 10:00:00 +0530

    feat: initial SQL generation script

---
+++ generate_sql.py
@@ -0,0 +1,12 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+tokenizer = T5Tokenizer.from_pretrained("t5-small")
+model = T5ForConditionalGeneration.from_pretrained("model")
+
+while True:
+    question = input("Enter question: ")
+    input_text = "translate English to SQL: " + question
+    input_ids = tokenizer.encode(input_text, return_tensors="pt")
+    outputs = model.generate(input_ids, max_length=100)
+    sql = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print("Generated SQL:", sql)
